c22603fef55c9ff66ac592dd25fe56cc2113708b
johnnadratowski <john@unifiedsocial.com>
2015-09-29 11:32:14 -0400

    Added word finder

---
+++ clojure/find_words/project.clj
@@ -0,0 +1,9 @@
+(defproject find_words "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main find-words.core
+  :aot [find-words.core]
+  :profiles {:uberjar {:aot :all}})
